f9378da7064945ac0c2728d021d3d30c41e381bf
gojohnnygo <gojnygo@gmail.com>
2015-04-15 17:22:26 -0700

    Completed 'get' from / url

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,6 +1,9 @@
 var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
+var urlParser = require('url');
+var request = require("http-request");
+var utils = require("./utils");
 
 // Why do you think we have this here?
 // HINT:It has to do with what's in .gitignore
@@ -8,6 +11,24 @@ initialize();
 
 var port = 8080;
 var ip = "127.0.0.1";
+
+// var routes = {
+//   '/': require('./request-handler').requestHandler
+// };
+
 var server = http.createServer(handler.handleRequest);
+
 console.log("Listening on http://" + ip + ":" + port);
+
 server.listen(port, ip);
+
+// request.get('www.google.com', function(err, res) {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   console.log(res.code, res.headers, res.buffer.toString());
+// });
+//
+
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,49 @@
+var fs = require('fs');
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var utils = require('./utils');
+var urlParser = require('url');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var objectId = 1;
+var messages = [];
+
+var actions = {
+  'GET': function(request, response){
+    utils.sendResponse(response, {results: messages});
+  },
+  'POST': function(request, response){
+    utils.collectData(request, function(message){
+      message.objectId = ++objectId;
+      messages.push(message);
+      utils.sendResponse(response, {objectId: 1}, 201);
+    });
+  },
+  'OPTIONS': function(request, response){
+    utils.sendResponse(response);
+  }
+};
+
+exports.handleRequest = function(request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+
+  var parts = urlParser.parse(request.url);
+  // console.log('parts: ', parts);
+
+  if (request.method === "GET") {
+
+    if (request.url === "/") {
+      fs.readFile(archive.paths.siteAssets + "/index.html", function (err, data){
+        response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        response.write(data);
+        response.end();
+      });
+    }
+    else {
+        utils.sendResponse(response, "Not Found", 404);
+    }
+
+
+
+  }
 };
